d9611635ea1a705db29f6d1e7e0421a0637f30a9
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-07-06 12:05:17 +0200

    waiting...

--- hw.sh
+++ hw.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
 LIMIT_WAIT_TIME=100
 NEXT_WAIT_TIME=0
-until command || [ $NEXT_WAIT_TIME -eq $LIMIT_WAIT_TIME ]; do
+until [ $NEXT_WAIT_TIME -eq $LIMIT_WAIT_TIME ]; do
    sleep $(( NEXT_WAIT_TIME++ ))
    echo "HELLO WOLRD $NEXT_WAIT_TIME"
 done
