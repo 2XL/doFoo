0ef26e0429b09d072bb65034b86e23307568735d
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-07-06 12:03:40 +0200

    Ultimate Commit

---
+++ hw.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+LIMIT_WAIT_TIME=100
+NEXT_WAIT_TIME=0
+until command || [ $NEXT_WAIT_TIME -eq $LIMIT_WAIT_TIME ]; do
+   sleep $(( NEXT_WAIT_TIME++ ))
+   echo "HELLO WOLRD $NEXT_WAIT_TIME"
+done
