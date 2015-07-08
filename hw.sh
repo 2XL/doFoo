4bce363565d9271e9cffd5dc74f882c00cb3f71f
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-07-08 10:07:30 +0200

    Added, config file and output logfile

--- hw.sh
+++ hw.sh
@@ -5,4 +5,5 @@ NEXT_WAIT_TIME=0
 until [ $NEXT_WAIT_TIME -eq $LIMIT_WAIT_TIME ]; do
    sleep $(( NEXT_WAIT_TIME++ ))
    echo "HELLO WOLRD $NEXT_WAIT_TIME" >> log
+   echo -e $NEXT_WAIT_TIME"s \t->" `cat config.cfg` " -->" `date`
 done
