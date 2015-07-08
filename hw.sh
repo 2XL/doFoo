962a99e09f5b85da31bf352449cb90e0fe7228d7
YaoMingJunior <xl16salou@gmail.com>
2015-07-08 09:58:47 +0200

    Update hw.sh
    Report output to log

--- hw.sh
+++ hw.sh
@@ -4,5 +4,5 @@ LIMIT_WAIT_TIME=100
 NEXT_WAIT_TIME=0
 until [ $NEXT_WAIT_TIME -eq $LIMIT_WAIT_TIME ]; do
    sleep $(( NEXT_WAIT_TIME++ ))
-   echo "HELLO WOLRD $NEXT_WAIT_TIME"
+   echo "HELLO WOLRD $NEXT_WAIT_TIME" >> log
 done
