543600688793472df9676252e20cd5aeb4c7a672
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-07-10 18:08:17 +0200

    update echo time and pwd format

--- hw.sh
+++ hw.sh
@@ -5,5 +5,5 @@ NEXT_WAIT_TIME=0
 until [ $NEXT_WAIT_TIME -eq $LIMIT_WAIT_TIME ]; do
    sleep $(( NEXT_WAIT_TIME++ ))
    # echo "HELLO WOLRD $NEXT_WAIT_TIME" >> log
-   echo -e $NEXT_WAIT_TIME"s \t->" `pwd` " -->" `date` >> log
+   echo -e $NEXT_WAIT_TIME"s \t->" $(pwd) " -->" $(date) >> log
 done
